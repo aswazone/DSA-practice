67456b27b09326421eeaa5c454a19b50397de6c3
aswazone <aswamanu143@gmail.com>
2025-01-20 11:44:00 +0530

    feat: DFS traversel

--- 16-tree.js
+++ 16-tree.js
@@ -46,21 +46,50 @@ class BinarySearchTree {
         }
     }
 
-   
+    // DFS traversal
+    preOrder(root){
+        if(root){
+            console.log(root.data);
+            this.preOrder(root.left);
+            this.preOrder(root.right);
+        }
+    }
+
+    inOrder(root){
+        if(root){
+            this.inOrder(root.left);
+            console.log(root.data);
+            this.inOrder(root.right);
+        }
+    }
+    postOrder(root){
+        if(root){
+            this.postOrder(root.left);
+            this.postOrder(root.right);
+            console.log(root.data);
+        }
+    }
+    
+
 }
 
 
 const newBst = new BinarySearchTree()
 
 newBst.insert(10)
-newBst.insert(2)
+newBst.insert(5)
+newBst.insert(3)
 newBst.insert(15)
-newBst.insert(-1)
+newBst.insert(7)
 console.log(newBst.search(newBst.root,15))
 console.log(newBst.search(newBst.root,10))
 console.log(newBst.search(newBst.root,2))
 console.log(newBst.search(newBst.root,-1))
 console.log(newBst.search(newBst.root,20))
+
+newBst.preOrder(newBst.root)
+newBst.inOrder(newBst.root)
+newBst.postOrder(newBst.root)
 
 console.log(newBst)
 
